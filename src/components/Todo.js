f9972f8dcb2e7e0d9315abb24ea60d30ccb433b9
Weslley.rangel <Weslley-rangel@outlook.com>
2020-09-28 17:41:26 -0300

    Ajuste o App.css para dar visibilidade no projeto

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useRef } from 'react';
 import TodoForm from './TodoForm';
 import { RiCloseCircleLine } from 'react-icons/ri'
 import { TiEdit } from 'react-icons/ti'
